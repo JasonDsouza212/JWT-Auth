072f7b0b3bd49aa1b46382f1c4243f4c13282488
Your Name <you@example.com>
2023-06-21 17:44:48 +0530

    login and sigout user with hooks

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom'
+import {useLogout} from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
+  const {logout}= useLogout()
+  const {user}= useAuthContext()
+
+  const handleClick =()=>{
+    logout()
+  }
 
   return (
     <header>
@@ -9,10 +17,20 @@ const Navbar = () => {
           <h1>Workout Buddy</h1>
         </Link>
         <nav>
+        {user &&(
           <div>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
+            <span>{user.email}</span>
+            <button onClick={handleClick}>
+                Log out
+            </button>
           </div>
+        )}
+        {!user &&(
+          <div>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </div>
+        )}
         </nav>
       </div>
     </header>
